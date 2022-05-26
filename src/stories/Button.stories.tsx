170acecc30ea3bbbddfac026fca6a49371b33d73
emewjin <emewjin@gmail.com>
2022-05-26 21:57:45 +0900

    button story

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,41 +1,49 @@
 import React from 'react';
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { MANTINE_SIZES, MANTINE_COLORS } from '@mantine/styles';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { Button } from '@mantine/core';
 
-import { Button } from './Button';
-
-// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: 'Example/Button',
+  title: 'Component/Button',
   component: Button,
-  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
-    backgroundColor: { control: 'color' },
+    size: {
+      control: {
+        type: 'select',
+        options: MANTINE_SIZES,
+      },
+    },
+    variant: {
+      control: {
+        type: 'select',
+        options: [
+          'filled',
+          'outline',
+          'light',
+          'gradient',
+          'white',
+          'default',
+          'subtle',
+        ],
+      },
+    },
+    color: {
+      control: {
+        type: 'select',
+        options: MANTINE_COLORS,
+      },
+    },
   },
 } as ComponentMeta<typeof Button>;
 
-// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
-const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
+const Template: ComponentStory<typeof Button> = (args) => (
+  <Button {...args}>버튼</Button>
+);
 
-export const Primary = Template.bind({});
-// More on args: https://storybook.js.org/docs/react/writing-stories/args
-Primary.args = {
-  primary: true,
-  label: 'Button',
-};
+export const Basic = Template.bind({});
 
-export const Secondary = Template.bind({});
-Secondary.args = {
-  label: 'Button',
-};
-
-export const Large = Template.bind({});
-Large.args = {
-  size: 'large',
-  label: 'Button',
-};
-
-export const Small = Template.bind({});
-Small.args = {
-  size: 'small',
-  label: 'Button',
+Basic.args = {
+  variant: 'filled',
+  size: 'md',
+  color: 'teal',
 };
